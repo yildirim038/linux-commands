0b34e89c632484ce8d4b1c651e99cd839b0a82a1
Ali Osman YILDIRIM <aosmanyildirim@gmail.com>
2020-12-19 16:32:48 +0100

    ilk

---
+++ shell_commands.sh
@@ -0,0 +1,16 @@
+Hangi klasörde oldugumuzu pwd yazarak kontol ederiz.
+Gizli dosyalar dahil olmak üzere tüm dosyalari göstermek icin ls -a kullanilir.
+Yeni klasör olusturmak icin mkdir kullanilir (mkdir yeniklasör) mkdir yazdiktan sonra klasör adi yazilir.
+Yeni bir dosya olusturmak icin touch kullanilir,touch yazdiktan sonra dosyanin adi yazilir. Dosyanin türü belirtmek icin dosya adindan sonra nokra ve dosya türü yazilabilir ancak zorunlu degildir.(touch yenibelge.sh veya touch yenibelge)
+Bir Dosyayi klasörün icine kopyalmak icin cp kullanilir.(cp yenibelge yeniklasör)
+cd ile bulunan klasör degistirilir, (cd yenikalsör)
+rm dosya adi girilerek olsuturulan bir dosya silinir.(rm yenibelge)
+echo "Birinci dosya temel shell komutlari" >> yenidosya yazarak yeni dosya icerisinde tirnak icerisinde yer alan bilginin oldugu yeni bir dosya olusturulur.
+cat yenidosya yazarak yenidosya adindaki dosyanin icerigini ekranda görüntüleye biliriz.
+grep shell yenidosya yazarak dosyanin iceriginde kelimenin gecip gecmedigi aranabilir.
+echo "Ikinci dosya disk islemleri" >> dosya2 yazarak ikinci bir dosya olusturdum.
+cat yenidosya dosya2 >> birlestirilendosya yazarak daha önce olusturdugumuz iki dosyayi tek bir dosya icinde birlestirebiliriz.
+cd .. ile bir üst klasöre gecilir.
+rm -r yeniklasör yazarak ici dolu olan yeniklasör adindaki klasörü icerigi ile virlikte silebiliriz.
+sudo su ile root a gecis yapilabilir.
+
