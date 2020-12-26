548214bef8feddc51718a3b839c411fa27c9cb3a
Ali Osman YILDIRIM <aosmanyildirim@gmail.com>
2020-12-27 00:45:01 +0100

    linux commands packagres editors

---
+++ linux-commands-packagres-editors.js
@@ -0,0 +1,29 @@
+/**
+ * sudo apt update ile günceleme yaptim.
+ * sudo apt upgrade ile kurulu programlari günceledim.
+ * sudo apt install code ile Visual Studio Code programini kurdum.
+ * wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb yazarak google chrome bilgisayara indirildi ve 
+ * sudo apt install ./google-chrome-stable_current_amd64.deb yazarak chorme yüklendi.
+ * sudo apt install libreoffice yazarak LibreOffice'i yüklemesi yapildi
+ * LibreOffice programinda "Writer" ile Word (.docx) formatinda,  "Calc" programi ile Excel (.xlsx) ve "Impress" ile yeni bir belgeyi Powerpoint (.pptx) dosya kaydetim.(kaydetme sirasinda tür degistirerek)
+ * sudo adduser ali ile adi ali olan yeni kullanici tanimladim.
+ * sudo addgroup aile ile aile adinda yeni bir grup olusturdum.
+ * vim index.html yazarak bir html klasörü olsuturduk i basarak etkinlestirip yazi yazdik ve son olarak esc basip :wq yapip kaydedip ciktik.
+ * nano app.js yazarak app.js olusturduk. ctrl o ile kaydetim ve ctrl x ile cikis yaptim
+ * sudo chown ali:aile ./app.js yazarak app.js klasörünün kullanim yetkisi ve grubunu degistirdim.
+ * chmod u=rwx,og=rwx index.html yazarak index.html dosyasinin okuma, yazma ve çalistirma izinlerini degisrieim.(user, group ve others hepsi icin izin verdim.)
+ * ls-l komutunu yazarak yaptigim degisiklikleri kontrol ettim. 
+ * alias komutu uzun komutlara kisayol atamaya yarar.
+ * df diskin ne karinin bos oldugunu nedadarinin kullanildigini gösteren komut.
+ * history bizim girdigimiz terminal komutlarini gösterir.
+ * date komutu serverin saat ve tarihini bize veriyor.
+ * ps komutu bilgisayarda suan hangi islemlerin yapildigini bize gösterir.Windows daki görevyöneticinde ki gibi.
+ * kill komutu windowsda ki görevi sonandir ile ayni kitlenen bir programi veya kaptilmak istenen programi kapatmaya yarar.
+ * tar komutu bir dosyayi sikistirmaya (ziplemeye)yarar.
+ * top komutu cpu nekadari kullaniliyor, memory i hangi program fazla kullaniyor gibi bilgileri bize veren komut.
+ * du komutu bulunfugumuz yerdeki klasörler ne kadar yer kapladigini gösteriyor.
+ * head komutu bir metin belgesinin ilk 10 satirini göstermeye yarar.
+ * tail komutu bir belgenin son 10 satirini görmeye yarar
+ * wget komutu terminaldan internet üzerinden dosya indirmeye yararyan bir komuttur.
+ * find komutu arama islemlerinde kullanilan bir komuttur.
+ */
